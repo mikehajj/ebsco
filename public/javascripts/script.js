0d37225a35e4da0d54de9f4af065de679ad0347f
mikehajj <mike.hajj@gmail.com>
2019-10-25 12:59:42 +0300

    updated the initialization of the frontend suggestions syndicator module and added support to expose and override default inputs names and API values.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,7 +8,7 @@ const suggestions = {
 	 * if the keywords length in the input is 2 and above, do search.
 	 */
 	suggest: () => {
-		let keywords = jQuery( "#keywords" ).val();
+		let keywords = jQuery( `#${this.config.inputName}` ).val();
 		if ( keywords.length >= 2 ) {
 			suggestions.autocomplete( keywords );
 		}
@@ -20,8 +20,8 @@ const suggestions = {
 	 */
 	autocomplete: ( keywords ) => {
 		let requestOptions = {
-			method: "POST",
-			url: "http://localhost:3000/autocomplete",
+			method: this.config.API.method.toUpperCase(),
+			url: this.config.API.uri,
 			data: { q: keywords }
 		};
 		
@@ -55,13 +55,13 @@ const suggestions = {
 	 */
 	reset: ( keywords ) => {
 		if ( keywords && keywords.length > 0 ) {
-			jQuery( "#keywords" ).val( keywords );
+			jQuery( `#${this.config.inputName}` ).val( keywords );
 		}
 		else {
-			keywords = jQuery( "#keywords" ).val();
+			keywords = jQuery( `#${this.config.inputName}` ).val();
 		}
-		jQuery( "#searchOutput" ).html( "" );
-		jQuery( "#suggestions" ).html( "" );
+		jQuery( `#${this.config.searchBoxName}` ).html( "" );
+		jQuery( `#${this.config.suggestionListName}` ).html( "" );
 		suggestions.suggest( keywords );
 	},
 	
@@ -76,12 +76,76 @@ const suggestions = {
 		else {
 			jQuery( el ).attr( 'class', 'list-group-item list-group-item-action active' );
 		}
+	},
+	
+	/**
+	 * Triggers deploying the suggestion syndicator
+	 */
+	run: () => {
+		let suggestionBox = `<div class="input-group mb-3">
+																<!-- input that holds the keywords to search for -->
+																<input type="text" id="${this.config.inputName}" class="form-control" placeholder="Type to search ..." />
+																<!-- input that holds the button to execute search on click events -->
+																<div class="input-group-append">
+																	<button class="btn btn-primary-secondary btn-primary" type="button" id="${this.config.searchName}">${this.config.searchLabel}</button>
+																</div>
+															</div>
+															<!-- suggestions container -->
+															<div id="${this.config.suggestionListName}" class="list-group form-control"></div>
+															<!-- box that contains the error division message -->
+															<div id="${this.config.searchBoxName}"></div>`;
+		//bootstrap the html
+		jQuery(`#${this.config.container}`).html(suggestionBox);
+		
+		//autocomplete
+		jQuery( `#${this.config.inputName}` ).keyup( function ( e ) {
+			suggestions.reset();
+		} );
+		
+		//trigger autocomplete if the user fills the input and hits enter.
+		jQuery( `#${this.config.inputName}` ).keypress( function ( e ) {
+			if ( e.which == 13 ) {
+				suggestions.reset();
+			}
+		} );
+		
+		//trigger autocomplete if the user clicks on the search button
+		jQuery( `#${this.config.searchName}` ).click( () => {
+			suggestions.reset();
+		} );
+	},
+	
+	/**
+	 * Initialize the main frontend plugin configuration object.
+	 * Store the name of the inputs in the class configuration object
+	 */
+	init: () => {
+		let options = null;
+		if(window.suggestionsOptions !== undefined && typeof window.suggestionsOptions === 'object' && Object.keys(window.suggestionsOptions).length > 0){
+			options = window.suggestionsOptions;
+		}
+		
+		this.config = {
+			container: (options) ? options.container : 'syndicator',
+			inputName: (options) ? options.inputName : 'keywords',
+			searchName: (options) ? options.searchName : 'search',
+			searchLabel: (options) ? options.searchLabel : 'Search',
+			suggestionListName: (options) ? options.suggestionListName : 'suggestions',
+			searchBoxName: (options) ? options.searchBoxName : 'searchOutput',
+			API: {
+				method: ( options && options.API && options.API.method) ? options.API.method : "POST",
+				uri: ( options && options.API && options.API.uri) ? options.API.uri : "http://localhost:3000/autocomplete"
+			}
+		};
+		
+		suggestions.run();
 	}
 	
 };
 
 /**
  * Method that prints an error message in a red division using bootstrap error tag.
+ * @param code {Number}
  * @param errorRepsonseObj {Object}
  */
 function printError( code, errorRepsonseObj ) {
@@ -91,44 +155,12 @@ function printError( code, errorRepsonseObj ) {
 		console.log( "Error: ", errorRepsonseObj );
 	}
 	
-	jQuery( "#searchOutput" ).append( `<div class="alert alert-danger text-center" role="alert">Something went wrong while executing your query. Please try again and if the problem persists, contact the Administrator.</div>` );
+	jQuery( "#searchOutput" ).append( `<div class="alert alert-danger text-center" role="alert"><b>${code}:</b>&nbsp;Something went wrong while executing your query. Please try again and if the problem persists, contact the Administrator.</div>` );
 }
 
 /**
  * Attach listeners to inputs once the document has finished loading.
  */
 jQuery( document ).ready( () => {
-	
-	let suggestionBox = `<div class="input-group mb-3">
-																<!-- input that holds the keywords to search for -->
-																<input type="text" id="keywords" class="form-control" placeholder="Type to search ..." />
-																<!-- input that holds the button to execute search on click events -->
-																<div class="input-group-append">
-																	<button class="btn btn-primary-secondary btn-primary" type="button" id="search">Search</button>
-																</div>
-															</div>
-															<!-- suggestions container -->
-															<div id="suggestions" class="list-group form-control"></div>
-															<!-- box that contains the error division message -->
-															<div id="searchOutput"></div>`;
-	//bootstrap the html
-	jQuery('#syndicator').html(suggestionBox);
-	
-	//autocomplete
-	jQuery( '#keywords' ).keyup( function ( e ) {
-		suggestions.reset();
-	} );
-	
-	//trigger autocomplete if the user fills the input and hits enter.
-	jQuery( '#keywords' ).keypress( function ( e ) {
-		if ( e.which == 13 ) {
-			suggestions.reset();
-		}
-	} );
-	
-	//trigger autocomplete if the user clicks on the search button
-	jQuery( "#search" ).click( () => {
-		suggestions.reset();
-	} );
-	
+	suggestions.init();
 } );
