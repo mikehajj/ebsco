d1148c7b7ad2518a26dcc4ee4d8d50e0d6723f6b
mikehajj <mike.hajj@gmail.com>
2019-10-31 13:00:09 +0200

    Created and Completed and tested aws lambda file.

--- lambda.js
+++ lambda.js
@@ -1,13 +1,18 @@
+/***********************************************************************
+ *
+ * AWS Lambda support
+ *
+ ***********************************************************************/
 "use strict";
 
 const path = require( "path" );
 const async = require( "async" );
 
-const config = require( path.join( __dirname, '/../', 'config', 'index' ) );
+const config = require( path.join( __dirname, 'config', 'index' ) );
 const logger = new require( 'bunyan' )( config.logger );
 
-const businessLogic = require( path.join( __dirname, '/../', 'lib', 'bl' ) );
-const response = require( path.join( __dirname, '../', 'lib', 'response' ) );
+const businessLogic = require( path.join( __dirname, 'lib', 'bl' ) );
+const response = require( path.join( __dirname, 'lib', 'response' ) );
 
 const Validator = require( 'jsonschema' ).Validator;
 var validate = new Validator();
@@ -15,7 +20,7 @@ var validate = new Validator();
 /**
  * Specific route that handles the autocomplete operation using serverless architecture
  */
-exports.autocomplete = ( event ) => {
+exports.autocomplete = ( event, context, callback ) => {
 	
 	const tasks = {
 		/**
@@ -23,6 +28,13 @@ exports.autocomplete = ( event ) => {
 		 * @param next {Function}
 		 */
 		"trapRequestAndInject": ( next ) => {
+			if ( event && !event.body && event.data ) {
+				event.body = event.data;
+			}
+			else if ( event && !event.body && event.q ) {
+				event.body = JSON.parse( JSON.stringify( event ) );
+				delete event.q;
+			}
 			event.log = logger;
 			event.config = config;
 			return next();
@@ -34,6 +46,7 @@ exports.autocomplete = ( event ) => {
 		 * @returns {*}
 		 */
 		"imfv": ( next ) => {
+			event.log.debug( `Validating arriving inputs from:`, JSON.stringify( event.body ) );
 			
 			//check if the inputs have the correct schema
 			let validationResult = validate.validate( event.body, config.schemas[ '/autocomplete' ] );
@@ -86,27 +99,27 @@ exports.autocomplete = ( event ) => {
 		
 	};
 	
-	return new Promise( resolve => {
-		async.series( tasks, ( error, data ) => {
-			
-			let apiReponse = {
-				statusCode: 0,
-				body: '',
-			};
-			
-			let output = {};
-			
-			if ( error ) {
-				output = response.isError( error.code, error.message );
-			}
-			else {
-				output = response.isData( data[ 'invokeEngine' ] );
-			}
-			
-			apiReponse.statusCode = output.code;
-			apiReponse.headers = output.header;
-			apiReponse.body = JSON.stringify( output.response );
-			resolve( apiReponse );
-		} );
+	async.series( tasks, ( error, data ) => {
+		
+		let apiReponse = {
+			statusCode: 0,
+			body: '',
+		};
+		
+		let output = {};
+		
+		if ( error ) {
+			output = response.isError( error.code, error.message );
+		}
+		else {
+			output = response.isData( data[ 'invokeEngine' ] );
+		}
+		
+		apiReponse.statusCode = output.code;
+		apiReponse.headers = output.header;
+		apiReponse.headers[ 'Access-Control-Allow-Origin' ] = "*";
+		apiReponse.headers[ 'Access-Control-Allow-Methods' ] = "OPTIONS,POST,GET";
+		apiReponse.body = JSON.parse( output.response );
+		return callback( null, apiReponse );
 	} );
 };
